2d5395786afb1a53e5fc285c8abe36b6fee02b48
mgd666 <1291788563@qq.com>
2023-11-16 01:03:56 -0800

    Finish tabBar

---
+++ sweetpet/lib/constant/pages.dart
@@ -0,0 +1,7 @@
+class Pages {
+  static const String init = "/init";
+  static const String home = "/home";
+  static const String health = "/health";
+  static const String message = "/message";
+  static const String mine = "/mine";
+}
