cc6e65bfbd2a706b168c45db73b3f998f941c7ca
mgd666 <1291788563@qq.com>
2023-11-19 00:40:35 -0800

    finish Post Part and create mock data

--- sweetpet/lib/api/api_client.dart
+++ sweetpet/lib/api/api_client.dart
@@ -5,9 +5,25 @@ class ApiClient {
   factory ApiClient() => _instance;
   static final ApiClient _instance = ApiClient._internal();
 
-  // 获取首页"发现"数据
+  // MainView Data
   Future getIndexData() async {
     await Future.delayed(const Duration(seconds: 1));
     return await Future.value(FakeData.indexData);
+  }
+
+  // Post Detail Data
+  Future getIndexDetailDataById(int id) async {
+    await Future.delayed(const Duration(seconds: 1));
+    for (var v in FakeData.cardDetailDataList) {
+      if (v.id == id) {
+        return await Future.value(v);
+      }
+    }
+    return null;
+  }
+
+  Future getCommentList() async {
+    await Future.delayed(const Duration(seconds: 1));
+    return await Future.value(FakeData.commentList);
   }
 }
--- sweetpet/lib/page/vc_router.dart
+++ sweetpet/lib/page/vc_router.dart
@@ -1,9 +1,11 @@
 import 'package:sweetpet/constant/pages.dart';
 import 'package:sweetpet/page/home_page/home_page.dart';
 import 'package:get/get.dart';
+import 'package:sweetpet/page/post_page/post_page.dart';
 
 class Routes {
   static final List<GetPage> getPages = [
     GetPage(name: Pages.home, page: () => HomePage()),
+    GetPage(name: Pages.indexDetail, page: () => PostPage()),
   ];
 }
