700c988b58614aefd744597e1bd83544b08565f9
xuan7678 <xuanxuan2980@gmail.com>
2023-11-27 11:28:20 -0800

    mall pages

---
+++ sweetpet/lib/page/mall_page/mall_page.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:sweetpet/controller/home_controller/home_controller.dart';
+import 'package:sweetpet/page/mall_page/pet_shop_page.dart';
+
+
+class MallPage extends StatelessWidget {
+  MallPage({Key? key}) : super(key: key);
+  final HomeController homeController = Get.put(HomeController());
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Pet Discovery Page',
+      home: PetDiscoveryPage(),
+    );
+  }
+}
+
+class PetDiscoveryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mall'),
+        leading: IconButton(
+          icon: Icon(Icons.arrow_back),
+          onPressed: () {
+            // Handle back button
+          },
+        ),
+        actions: [
+          IconButton(
+            icon: Icon(Icons.shopping_cart),
+            onPressed: () {
+              // Handle shopping cart button
+            },
+          ),
+        ],
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: TextFormField(
+              decoration: InputDecoration(
+                hintText: 'Find the best for your pet...',
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(30.0),
+                ),
+              ),
+            ),
+          ),
+          Expanded(
+            child: ListView(
+              children: <Widget>[
+                PetCategoryWidget(
+                  imageName: 'assets/images/dog.png',
+                  categoryName: 'Dog',
+                ),
+                PetCategoryWidget(
+                  imageName: 'assets/images/cat.png',
+                  categoryName: 'Cat',
+                ),
+                // Add more categories as needed
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PetCategoryWidget extends StatelessWidget {
+  final String imageName;
+  final String categoryName;
+
+  const PetCategoryWidget({
+    Key? key,
+    required this.imageName,
+    required this.categoryName,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      leading: Image.asset(
+        imageName,
+        width: 100,
+        height: 100,
+      ),
+      title: Text(
+        categoryName,
+        style: TextStyle(
+          fontWeight: FontWeight.bold,
+          fontSize: 24.0,
+        ),
+      ),
+      onTap: () {
+        // Handle tap
+        Navigator.push(
+          context,
+          MaterialPageRoute(builder: (context) => PetShopPage(name: categoryName,)),
+        );
+      },
+    );
+  }
+}
---
+++ sweetpet/lib/page/mall_page/pet_shop_page.dart
@@ -0,0 +1,90 @@
+import 'package:flutter/material.dart';
+
+class PetShopPage extends StatelessWidget {
+
+  final String name;
+
+  PetShopPage({
+    Key? key,
+    required this.name,
+  }) : super(key: key);
+
+
+  final List<PetProduct> products = [
+    PetProduct(imageUrl: 'assets/images/dog_food.png', name: 'Food'),
+    PetProduct(imageUrl: 'assets/images/dog_food.png', name: 'Treats'),
+    PetProduct(imageUrl: 'assets/images/dog_food.png', name: 'Toys'),
+    PetProduct(imageUrl: 'assets/images/dog_food.png', name: 'Treatment'),
+    // 添加更多产品
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Mall'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: TextField(
+              decoration: InputDecoration(
+                border: OutlineInputBorder(),
+                hintText: 'Find the best for your pet...',
+              ),
+            ),
+          ),
+          Expanded(
+            child: GridView.builder(
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                crossAxisCount: 2,
+                childAspectRatio: 0.8,
+              ),
+              itemCount: products.length,
+              itemBuilder: (context, index) {
+                return PetProductWidget(product: products[index]);
+              },
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PetProduct {
+  final String imageUrl;
+  final String name;
+
+  PetProduct({required this.imageUrl, required this.name});
+}
+
+class PetProductWidget extends StatelessWidget {
+  final PetProduct product;
+
+  const PetProductWidget({Key? key, required this.product}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Column(
+        children: <Widget>[
+          Expanded(
+            child: Image.asset(
+              product.imageUrl,
+              fit: BoxFit.cover,
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.symmetric(vertical: 4.0),
+            child: Text(
+              product.name,
+              style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
