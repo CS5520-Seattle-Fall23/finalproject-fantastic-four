9df892c8a1da0188dd86c2b8c0074c31326facff
Tantantan-py <exactbrycetan@gmail.com>
2023-12-03 11:53:54 -0800

    add gallery option

--- sweetpet/lib/page/chat_page/user_image_picker.dart
+++ sweetpet/lib/page/chat_page/user_image_picker.dart
@@ -5,9 +5,9 @@ import 'package:image_picker/image_picker.dart';
 
 class UserImagePicker extends StatefulWidget {
   const UserImagePicker({
-    super.key,
+    Key? key,
     required this.onPickImage,
-  });
+  }) : super(key: key);
 
   final void Function(File pickedImage) onPickImage;
 
@@ -20,9 +20,9 @@ class UserImagePicker extends StatefulWidget {
 class _UserImagePickerState extends State<UserImagePicker> {
   File? _pickedImageFile;
 
-  void _pickImage() async {
+  Future<void> _pickImage(ImageSource source) async {
     final pickedImage = await ImagePicker().pickImage(
-      source: ImageSource.camera,
+      source: source,
       imageQuality: 50,
       maxWidth: 150,
     );
@@ -37,7 +37,7 @@ class _UserImagePickerState extends State<UserImagePicker> {
   }
 
   @override
-  build(context) {
+  Widget build(BuildContext context) {
     return Column(
       children: [
         CircleAvatar(
@@ -46,15 +46,30 @@ class _UserImagePickerState extends State<UserImagePicker> {
           foregroundImage:
               _pickedImageFile != null ? FileImage(_pickedImageFile!) : null,
         ),
-        TextButton.icon(
-          onPressed: _pickImage,
-          icon: const Icon(Icons.image),
-          label: Text(
-            'Add Image',
-            style: TextStyle(
-              color: Theme.of(context).colorScheme.primary,
+        Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextButton.icon(
+              onPressed: () => _pickImage(ImageSource.camera),
+              icon: const Icon(Icons.camera),
+              label: Text(
+                'Take Photo',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
             ),
-          ),
+            TextButton.icon(
+              onPressed: () => _pickImage(ImageSource.gallery),
+              icon: const Icon(Icons.image),
+              label: Text(
+                'Choose from Gallery',
+                style: TextStyle(
+                  color: Theme.of(context).colorScheme.primary,
+                ),
+              ),
+            ),
+          ],
         ),
       ],
     );
