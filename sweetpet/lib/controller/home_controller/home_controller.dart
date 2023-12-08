d4cb3b2243a082d073d8091276a564aa607a7ef8
xt <exactbrycetan@gmail.com>
2023-12-08 02:10:12 -0800

    failed make new post on the top

--- sweetpet/lib/controller/home_controller/home_controller.dart
+++ sweetpet/lib/controller/home_controller/home_controller.dart
@@ -1,9 +1,35 @@
 import 'package:get/get.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:sweetpet/model/post_detail.dart';
 
 class HomeController extends GetxController {
   RxInt currentIndex = 0.obs;
+  RxList<PostDetail> posts = <PostDetail>[].obs;
 
   void onChangePage(int index) {
     currentIndex.value = index;
+  }
+
+  Future<void> getPosts() async {
+    try {
+      final QuerySnapshot<Map<String, dynamic>> postSnapshot =
+          await FirebaseFirestore.instance.collection('post').get();
+
+      posts.clear(); // Clear existing posts before adding new ones
+
+      postSnapshot.docs.forEach((DocumentSnapshot<Map<String, dynamic>> doc) {
+        final Map<String, dynamic> data = doc.data()!;
+        final post = PostDetail.fromJson(data);
+        posts.add(post);
+      });
+
+      // Sort the posts based on the date in descending order
+      posts.sort((a, b) => b.date.compareTo(a.date));
+
+      // Update the UI
+      update();
+    } catch (error) {
+      print('Error fetching posts: $error');
+    }
   }
 }
--- sweetpet/test/widget_test.dart
+++ sweetpet/test/widget_test.dart
@@ -7,24 +7,61 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:sweetpet/controller/login_controller/login_controller.dart';
 
 import 'package:sweetpet/main.dart';
 
 void main() {
-  testWidgets('Counter increments smoke test', (WidgetTester tester) async {
+  testWidgets('Login Page Widget Test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(const MyApp());
+    await tester.pumpWidget(LoginController());
 
-    // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    // Verify that the initial state is as expected.
+    expect(find.text('Welcome'), findsOneWidget);
+    expect(find.text('Join Us'), findsNothing);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    // Tap the "New User? Register Now!" button and trigger a frame.
+    await tester.tap(find.text('New User? Register Now!'));
     await tester.pump();
 
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    // Verify that the state is updated after tapping the button.
+    expect(find.text('Welcome'), findsNothing);
+    expect(find.text('Join Us'), findsOneWidget);
+
+    // Test for validating email and password inputs.
+    await tester.enterText(
+        find.byKey(const Key('emailTextField')), 'invalid-email');
+    await tester.enterText(find.byKey(const Key('passwordTextField')), 'short');
+    await tester
+        .tap(find.text('Log In')); // Try to log in with invalid credentials.
+    await tester.pump();
+    expect(find.text('Please enter a valid email address'), findsOneWidget);
+    expect(find.text('Password must be at least 6 characters long'),
+        findsOneWidget);
+
+    // Test the successful registration flow.
+    await tester.tap(find.text('New User? Register Now!'));
+    await tester.enterText(
+        find.byKey(const Key('emailTextField')), 'test@example.com');
+    await tester.enterText(
+        find.byKey(const Key('passwordTextField')), 'password123');
+    await tester.enterText(
+        find.byKey(const Key('usernameTextField')), 'testuser');
+    await tester.tap(find.text('Sign Up'));
+    await tester.pumpAndSettle(); // Wait for animations to complete.
+    expect(find.text('Welcome'), findsNothing);
+    expect(find.text('Join Us'), findsNothing);
+    // Add more assertions based on your application's behavior.
+
+    // Test error handling for authentication failure.
+    // This assumes you have an invalid login scenario in your app.
+    await tester.tap(find.text('I Already Have An Account'));
+    await tester.enterText(
+        find.byKey(const Key('emailTextField')), 'invalid@example.com');
+    await tester.enterText(
+        find.byKey(const Key('passwordTextField')), 'wrongpassword');
+    await tester.tap(find.text('Log In'));
+    await tester.pumpAndSettle(); // Wait for animations to complete.
+    expect(find.text('Authentication failed'), findsOneWidget);
   });
 }
