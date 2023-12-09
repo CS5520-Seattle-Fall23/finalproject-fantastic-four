920c68c785a81f8859125162921731ab980085c7
daniel66 <daniel66@uw.edu>
2023-12-08 17:38:04 -0800

    placeholder pages

---
+++ sweetpet/lib/page/health_page/sleep.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class SleepPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sleep Page'),
+      ),
+      body: Center(
+        child: Text('This is the Sleep Page'),
+      ),
+    );
+  }
+}
